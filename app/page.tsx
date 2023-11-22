8d2a8b1860ea3a85e02bcb4a5b0a0c723d941e39
Sharmaine Payong <sharmainerpayong@gmail.com>
2023-11-22 15:51:15 +0100

    Edit code

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import './globals.css';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
